654e62aa3d98ddb428fe3cae5b18d01467b4a856
ChenliangLi205 <ChenliangLi@nuaa.edu.cn>
2019-03-20 19:16:58 +0800

    20190320

--- IO/NewsCommentsProcessor.py
+++ IO/NewsCommentsProcessor.py
@@ -3,22 +3,20 @@ import socket
 class Processor(object):
     def __init__(self):
         self.mailBox = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.binded = False
 
     def BindMailBox(self, ipAddr, port):
         try:
             self.mailBox.bind((ipAddr, port))
         except:
             print("binding port %d failed" % port)
-            self.binded = False
+            return False
         else:
-            self.binded = True
+            return True
 
     def ReceivLetter(self):
-        if self.binded:
-            self.mailBox.listen(5)
-            postMan, addr = self.mailBox.accept()
-            letter = postMan.recv(1000)
-            postMan.close()
-            return letter.decode("utf-8")
-        return
+
+        self.mailBox.listen(5)
+        postMan, addr = self.mailBox.accept()
+        letter = postMan.recv(1000)
+        postMan.close()
+        return letter.decode("utf-8")
