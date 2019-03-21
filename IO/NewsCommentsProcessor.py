264f2f2609062b506b4ae20bb45445e9d4e5dc77
ChenliangLi205 <ChenliangLi@nuaa.edu.cn>
2019-03-21 22:19:46 +0800

    20190321

--- IO/NewsCommentsProcessor.py
+++ IO/NewsCommentsProcessor.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 class Receiver(object):
     def __init__(self, ipAddr, port):
@@ -10,3 +11,11 @@ class Receiver(object):
         response = b"OK"
         self.mailBox.sendto(response, addr)
         return data.decode("utf-8")
+
+    def ReceiveLetters(self, q):
+        # q: multiprocessing.Queue
+        while True:
+            data, addr = self.mailBox.recvfrom(1024)
+            q.put(data.decode("utf-8"))
+            response = b"OK"
+            self.mailBox.sendto(response, addr)
